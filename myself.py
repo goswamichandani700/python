1711b33039fc306261224be58f8ca0d322ebc6fc
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-03 12:35:15 +0530

    Add files via upload

---
+++ myself.py
@@ -0,0 +1,15 @@
+#create python program to store below data in variables
+name = "goswami chandani ashwinfgiri"
+email = "goswamichandani700@gmail.com"
+mobile = 9265089850
+age = 27
+weight = 65
+gender = False
+
+#display all variables.
+print(name) 
+print(email)
+print(mobile)
+print(age)
+print(weight)
+print(gender)
