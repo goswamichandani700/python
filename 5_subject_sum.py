4026195ffb6a77e1ccaaa68929861c2a1ed19e81
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-05 14:55:53 +0530

    Add files via upload

---
+++ 5_subject_sum.py
@@ -0,0 +1,13 @@
+#write a program to make sum of 3 user given 3 subject marks (english, maths,science) and display it
+
+english = input("Enter english")
+maths = input("Enter maths")
+science = input("Enter science ") 
+
+english = int(english)
+maths = int(maths)
+science = int(science)
+
+sum = english + maths + science
+print("subject marks of enlish,maths,science=",sum)
+
