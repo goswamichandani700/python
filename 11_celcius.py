96c4b841a9af82b918b7a3073ef8220b37317a67
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-06 13:00:48 +0530

    Add files via upload

---
+++ 11_celcius.py
@@ -0,0 +1,7 @@
+#write a program to convert celcius into farenhit
+#input
+celcius = input("Enter celcius")
+#convert
+celcius = float(celcius)
+farenhit = (celcius*1.8)+32
+print("farenhit",farenhit)
