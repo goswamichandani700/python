0ecf7b848c4cbe5e20c6f9dcbf5c7d308e75fdc3
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-07 13:04:23 +0530

    Add files via upload

---
+++ 15_inches.py
@@ -0,0 +1,13 @@
+#write a program to convert meters into inches.
+
+#input 
+meters = input("Enter meters")
+
+#convert
+meters = int(meters)
+
+#process
+inches = meters * 39.3701
+
+#output
+print("inches",inches)
