588827889f447d86824848e03d8c2d19ae4a7dcb
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-07 13:04:48 +0530

    Add files via upload

---
+++ 16_rupees.py
@@ -0,0 +1,13 @@
+#write a program to convert dollar into rupees.
+
+#input
+dollar = input("dollar")
+
+#convert
+dollar = int(dollar)
+
+#process
+rupees = dollar * 97
+
+#output
+print("rupees",rupees)
