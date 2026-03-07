9bfafa2db74d13f8ab795eb103b1ef34fb103f8a
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-07 13:03:10 +0530

    Add files via upload

---
+++ 14_foot.py
@@ -0,0 +1,13 @@
+#write a program to convert foot into meters
+
+#input 
+foot = input("Enter foot")
+
+#convert
+foot = int(foot)
+
+#process
+meters = foot * 0.3048
+
+#output
+print("meters",meters)
