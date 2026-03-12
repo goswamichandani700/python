d694ca7e793b0c29a570e95f5ad4ac1fc79594a5
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-12 13:31:13 +0530

    Add files via upload

---
+++ 30_ai.py
@@ -0,0 +1,9 @@
+#create tuple which has 10 LLM ai names.
+tuple = ('openAI','google','anthropic','meta','mistralai','cohere','alibaba','xai','deepseek','microsoft')
+print(tuple)
+
+print(tuple[0:5])
+
+print(tuple[5:])
+
+print(tuple[0:3])
