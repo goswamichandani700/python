635172daf51c5389b955af2382b3301daca51cd9
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-12 13:30:12 +0530

    Add files via upload

---
+++ 27_list.py
@@ -0,0 +1,29 @@
+marks = [50,80,40,90,35,100,90]
+print(marks)
+position = marks.index(100)
+print("position of 100",position)
+
+#position = marks.index(99)
+#print("position of 99",position)
+
+temp = marks.count(90)
+print("count of temp",temp)
+
+marks.sort()
+print(marks)
+
+marks.reverse()
+print(marks)
+
+marks.reverse()
+print(marks)
+
+
+
+#marks2 = marks #wrong way
+marks2 = marks.copy()
+
+marks2.clear()
+print(marks)
+print(marks2)
+
