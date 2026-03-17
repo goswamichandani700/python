34bfef2ee83550e940c0f9db33352e3d99476f05
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-17 15:26:23 +0530

    Add files via upload

---
+++ 39_dictionary.py
@@ -0,0 +1,12 @@
+#create dictionary store student name and marks for maths,science,english,computer,drawing
+ 
+marks = {'name':'chandani','maths':89,'science': 80,'english':89,'computer':89,'drawing':80}
+print(marks)
+
+sum = marks['maths'] +marks['science'] + marks['english'] + marks['computer'] + marks['drawing']
+print(sum)
+
+average = sum/5
+print(average)
+
+
