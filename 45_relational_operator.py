f9cb028c8576c7d2f8cd904bf5044aaa4e210fac
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-19 11:01:33 +0530

    Add files via upload

---
+++ 45_relational_operator.py
@@ -0,0 +1,24 @@
+# relatinal operators
+
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+
+result = num1 == num2 #false 10 == 20
+#false = 10 == 20
+#print(result,"=",num1,"==",num2)
+print(f"{result} = {num1} == {num2}")
+
+result = num1 != num2 #true
+print(f"{result} ={num1} != {num2}")
+
+result = num1 < num2
+print(f"{result} ={num1} < {num2}")
+
+result = num1 > num2
+print(f"{result} = {num1} > {num2}")
+
+result = num1 <= num2
+print(f"{result} = {num1} <= {num2}")
+
+result = num1 >= num2
+print(f"{result} = {num1} >= {num2}")
