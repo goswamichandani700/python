ba927dbfc3130c573ff761a17c4c4e740b669c27
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-20 14:48:12 +0530

    Add files via upload

---
+++ 51_if_3.py
@@ -0,0 +1,32 @@
+#write a program to display name of day from given day
+#day = 1 print monday
+#day = 2 print tuesday
+#day = 3 print wednesday
+#day = 4 print thursday
+#day = 5 print friday
+#day = 6 print saturday
+#day = 7 print sunday
+
+day = int(input("enter day"))
+
+if day == 1:
+    print("monday")
+
+if day == 2:
+    print("tuesday")
+
+if day == 3:
+    print("wednesday")        
+
+if day == 4:
+    print("thursday")
+
+if day == 5:
+    print("friday")
+
+if day == 6:
+    print("saturday")
+
+if day == 7:
+    print("sunday")
+                    
