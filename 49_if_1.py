2d1cb6dad9b549dae6beb227aa17221d516b541b
goswamichandani700 <goswamichandani700@gmail.com>
2026-03-20 14:45:49 +0530

    Add files via upload

---
+++ 49_if_1.py
@@ -0,0 +1,17 @@
+#write a program to findout whether given number is positive number.if no is positive then only display message number is positive number.
+
+number = int(input("enter number"))
+print(number)
+
+# == != < > <= >= 
+if number>0:
+    print("this number is positive number")
+    print("positive number are alway greater then 0")
+
+print("good bye")    
+  
+
+
+
+
+
