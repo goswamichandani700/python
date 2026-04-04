58a0993c7b0c0cedf14f202524681feb45615789
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-04 14:18:41 +0530

    Add files via upload

---
+++ 108_for_nested.py
@@ -0,0 +1,22 @@
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# only 1 astrik per print function 
+# print("*")
+for astrik in range(1,5):
+    for astrik in range(1,6):
+        print('*',end=' ')
+    print(' ')    
+    
+    
+
+    
+# for astrik in range(1,6):
+#     print("*", end=' ')
+# print('')    
+# for astrik in range(1,6):
+#     print("*", end=' ')    
+# print('')    
+# for astrik in range(1,6):
+#     print("*", end=' ')        
