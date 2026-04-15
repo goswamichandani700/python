2ab1f4c00c2dd5c5321a1babea9fa7167ebfa7fe
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-15 13:22:27 +0530

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,17 @@
+# create module currency.py that has functions  to convert given indian rupees into different currency
+#     toDollar()
+#     toEuro()
+#     toYen()
+#     toPound()
+
+def dollartorupees(dollar):
+    return dollar * 83
+
+def eurotorupees(euro):
+    return euro * 90
+
+def yentorupees(yen):
+    return yen * 0.55
+
+def poundtorupees(pound):
+    return pound * 105 
