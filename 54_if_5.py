5935288b2964adf477645aed707cbebe453b4e6c
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-20 10:45:49 +0530

    Add files via upload

--- 54_if_5.py
+++ 54_if_5.py
@@ -1,19 +1,18 @@
 # write a program to accept length and width of two different farm from user. and findout & display which farm is bigger 
 #input length and width
-length1 = int(input("enter length"))
-width1 = int(input("enter width"))
-
-# == != < > <= >= 
-area1 = length1 * width1
-if area1>0:
-    print(f"lenght1 and width1 {area1}")
-
+length1 = int(input("enter length1"))
+width1 = int(input("enter width1"))
 length2 = int(input("enter length2 "))    
 width2 = int(input("enter width2"))
-
+# == != < > <= >= 
+area1 = length1 * width1
 area2 = length2 * width2
-if area2>0:
-    print(f"length2 and width2 {area2}")
+if area1>area2:
+    print("area1 is bigger")
+if area2>area1:
+    print("area2 is bigger")
 
-area = area2>area1
-print(area)   
+
+
+
+
