f667ffc9a17bfc737ab7dd0c29aea49c623a91ef
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-20 10:55:00 +0530

    Delete 11_celcius.py

--- 11_celcius.py
+++
@@ -1,7 +0,0 @@
-#write a program to convert celcius into farenhit
-#input
-celcius = input("Enter celcius")
-#convert
-celcius = float(celcius)
-farenhit = (celcius*1.8)+32
-print("farenhit",farenhit)
