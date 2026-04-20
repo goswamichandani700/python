24fdf9a070640018eb53bab0a6847e6bce30ab4f
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-20 10:46:12 +0530

    Add files via upload

--- 55_if_6.py
+++ 55_if_6.py
@@ -4,12 +4,10 @@
 brother1 = int(input("enter age"))
 brother2 = int(input("enter age"))
     
-if brother1>0:
- print(f" age {brother1}")
-if brother2>0:
-    print(f"age{brother2}")
+if brother1>brother2:
+ print("brother1 is big")
+if brother2>brother1:
+    print("brother2 is big")
 
-elder = brother2 - brother1
-print(f"difference {elder}")
 
 
