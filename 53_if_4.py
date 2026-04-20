5089b261a17cf993fa24e4ae0569cde4d4a41d32
goswamichandani700 <goswamichandani700@gmail.com>
2026-04-20 13:02:28 +0530

    Add files via upload

--- 53_if_4.py
+++ 53_if_4.py
@@ -1,21 +1,14 @@
 #write a program to convert 24 hours time into 12 hours fomat time and display it with am pm message.
 
 
-hours = int(input("enter hours"))
 
-difference = hours - 12
-#== != < > <= >= 
-if difference>0:
-    print(f"format time with {difference}pm")
-
-difference = hours - 12
-# == != < > <= >=
-if difference>0:
-    print(f"format time with {difference}am ")
-
-difference = hours - 12
-if difference>0:
-    print(f"invald output{difference}")    
+hours = int(input("Enter hours"))
+if hours>12:
+    hours = hours - 12 
+    msg = ' PM'
+else:
+    msg = ' AM'
+print(f" {hours} {msg}")
 
 
     
