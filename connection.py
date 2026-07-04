21dc73d14344bf50d052f99868605eed48594cf2
goswamichandani700 <goswamichandani700@gmail.com>
2026-07-04 09:52:45 +0530

    Add files via upload

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import mysql.connector as connector
 #create connection 
 try:
     connect = connector.connect(host='localhost',user='root',passwd='',
-                                database='ai_ml_ds_11',port='3306')
+                                database='ai_ml_ds_11',port='3308')
     print('connection created.')
 except connector.Error as error:
     print("connection could not be created due to error,")
