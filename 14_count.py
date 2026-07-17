0edb0265f25f9d84be3d99a28c4eea3cc13b8e06
goswamichandani700 <goswamichandani700@gmail.com>
2026-07-17 12:29:09 +0530

    Add files via upload

---
+++ 14_count.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import pandas as pd 
+tips = sns.load_dataset('tips')
+sns.countplot(data=tips,x='sex')
+plt.xlabel("count of tips")
+plt.ylabel("Gender")
+plt.grid(which='both')
+plt.show()
